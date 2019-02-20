4d0f3b07ea3dcbf75088a81d27511012c4dc745c
Joanna Stiller <joannastiller@yahoo.com>
2019-02-20 15:32:20 +0100

    Initilal commit

---
+++ main.js
@@ -0,0 +1,17 @@
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+    e.target.classList.remove('playing');
+}
+
+function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    if (!audio) return; //stops the function for non-defined keys
+    key.classList.add('playing');
+    audio.currentTime = 0; //rewinds to the beginning
+    audio.play();
+}
+
+const keys = Array.from(document.querySelectorAll('.key'));
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', playSound);
